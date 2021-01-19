2d5c2d52ea8097f4105dc5e21cfaaaddd946a869
ahmethamdibayrak <ahmethamdibayrak@hotmail.com>
2021-01-19 23:08:20 +0300

    update

--- config/admin.php
+++ config/admin.php
@@ -2,7 +2,26 @@
 
 return [
 	'jqadm' => [
-	],
+        'post' => [
+            'standard' => [
+                'subparts' => [
+                    'media' => 'media',
+                    'text' => 'text',
+                ],
+            ]
+        ],
+        'navbar' => [
+            'post'=>['post']
+         ],
+        'resource' =>[
+            'post' => [
+                'groups' => ['admin', 'editor', 'super'],
+                'key' => 'SWP',
+            ],
+        ],
+    ],
 	'jsonadm' => [
+
 	],
 ];
+    
